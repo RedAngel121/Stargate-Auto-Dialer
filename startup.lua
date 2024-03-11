e9d9bbacc8f9f6379f60aea4a135a8307f8128b8
RedAngel121 <33588872+RedAngel121@users.noreply.github.com>
2024-03-11 15:14:09 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,3 +1,4 @@
-if not exists AddressList.lua then
-  create address list lua with return default {loc="Home" address={1,2,3,4,5,6,7,8}}
-now start the Dialer program
+if not fs.exists("AddressList.lua") then
+    saveFile("AddressList.lua", "return {\\n    {loc=\"Home\" address={1,2,3,4,5,6,7,8}\\n}")
+end
+shell.run("SGJ_AutoDialer")
