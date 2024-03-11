e2dae6ae68696827d118ecabbde2a18e3024c249
RedAngel121 <33588872+RedAngel121@users.noreply.github.com>
2024-03-11 14:30:40 -0400

    Update AddressList.lua

--- AddressList.lua
+++ AddressList.lua
@@ -1,3 +1,7 @@
 return {
-    { name = "Abydos",      address = { 1, 17, 2, 34, 26, 9, 33 } }
+    { locName = "My House", address = {27,9,19,35,32,2,10,0}},
+    { locName = "Abydos", address = {2,6,14,31,11,29,0}},
+    { locName = "Chulak", address = {9,1,2,14,36,19,0}},
+    { locName = "Moon Gate", address = {26,32,11,21,13,34,35,0}},
+    { locName = "Mars Gate", address = {24,1,20,8,26,25,33,0}}
 }
