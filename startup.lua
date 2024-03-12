89c00c13d15c812db8b46b9d545bb9988d49fad2
RedAngel121 <33588872+RedAngel121@users.noreply.github.com>
2024-03-12 03:49:13 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -2,4 +2,4 @@ if not fs.exists("AddressList.lua") then
     local f = io.open("AddressList.lua", "w")
     f:write("return {\n    {loc=\"Home\" address={1,2,3,4,5,6,7,8}}\n}")
 end
-shell.run("SGJ_Autodialer.lua")
+shell.run("dialer")
