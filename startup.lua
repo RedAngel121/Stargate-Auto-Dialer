29faa10d9aa0d62e8e7672a7c41057e0c7b254a2
RedAngel121 <33588872+RedAngel121@users.noreply.github.com>
2024-03-12 10:56:14 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,5 +1,5 @@
 if not fs.exists("AddressList.lua") then
     local f = io.open("AddressList.lua", "w")
-    f:write("return {\n    {loc=\"Home\" address={1,2,3,4,5,6,7,8,0}}\n}")
+    f:write("return {\n\t{loc=\"Home\" address={1,2,3,4,5,6,7,8,0}}\n}")
 end
 shell.run("dialer")
