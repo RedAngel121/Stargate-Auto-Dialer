042fd2b496ba02e7af34e482d15ed98a82693b46
RedAngel121 <33588872+RedAngel121@users.noreply.github.com>
2024-03-19 17:51:13 -0400

    Update dialer.lua

--- dialer.lua
+++ dialer.lua
@@ -2,9 +2,34 @@
 -- https://www.youtube.com/watch?v=qNi9NUAmOJM
 -- To navigate the menu please use W/S or the UP/DOWN arrows, Make your Selection using Enter.
 
-require("sal")
 local w,h = term.getSize()
 local nOption = 1
+
+-- Function to save the Address List to AddressList.lua
+function saveItemList()
+    local file = io.open("AddressList.lua", "w")
+    if file then
+        file:write("return {\n")
+        for _, item in ipairs(itemList) do
+            file:write(string.format("\t{locName=\"%s\", address={%s}},\n", item.locName, table.concat(item.address, ",")))
+        end
+        file:write("}\n")
+        file:close()
+    end
+end
+
+-- Function to load the Address List from AddressList.lua
+function loadItemList()
+    local file = io.open("AddressList.lua", "r")
+    if file then
+        local content = file:read("*all")
+        file:close()
+        local success, loadedItemList = pcall(load(content))
+        if success and type(loadedItemList) == "table" then
+            itemList = loadedItemList
+        end
+    end
+end
 
 function dial(address)
     printCenter(math.floor(h/2)-2, "Dialing Stargate Address")
