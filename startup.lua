b4d5dd09cc264789ccfe3a74a31231b4277d7e36
RedAngel121 <33588872+RedAngel121@users.noreply.github.com>
2024-03-19 12:32:58 -0400

    Update startup.lua
    Interface Check

--- startup.lua
+++ startup.lua
@@ -2,4 +2,12 @@ if not fs.exists("AddressList.lua") then
     local f = io.open("AddressList.lua", "w")
     f:write("return {\n\t{loc=\"Home\" address={1,2,3,4,5,6,7,8,0}}\n}")
 end
+interface = peripheral.find("basic_interface") or peripheral.find("crystal_interface") or peripheral.find("advanced_crystal_interface")
+if interface == nil then
+    term.clear()
+    term.setCursorPos(1, 1)
+    print("INTERFACE NOT FOUND:\nPlease Check your Modem's Connection\n\nComputer will automatically reboot in 5 seconds...")
+    sleep(5)
+    os.reboot()
+end
 shell.run("dialer")
