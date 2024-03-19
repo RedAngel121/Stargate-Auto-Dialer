903cecc0bc02ea177e204a221593099cd37cce84
RedAngel121 <33588872+RedAngel121@users.noreply.github.com>
2024-03-19 12:20:56 -0400

    Delete AddressList.lua
    File Not Needed

--- AddressList.lua
+++
@@ -1,7 +0,0 @@
-return {
-    { locName = "My House", address = {27,9,19,35,32,2,10,0}},
-    { locName = "Abydos", address = {2,6,14,31,11,29,0}},
-    { locName = "Chulak", address = {9,1,2,14,36,19,0}},
-    { locName = "Moon Gate", address = {26,32,11,21,13,34,35,0}},
-    { locName = "Mars Gate", address = {24,1,20,8,26,25,33,0}}
-}
