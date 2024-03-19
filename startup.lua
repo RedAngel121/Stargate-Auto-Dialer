d0f379c11daf5329313a6a1daf2be3b09770b0af
RedAngel121 <33588872+RedAngel121@users.noreply.github.com>
2024-03-19 19:45:06 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,6 +1,6 @@
 if not fs.exists("AddressList.lua") then
     local f = io.open("AddressList.lua", "w")
-    f:write("return {\n\t{loc=\"Home\" address={1,2,3,4,5,6,7,8,0}}\n}")
+    f:write("return {\n\t{locName=\"Home\", address={1,2,3,4,5,6,7,8,0}}\n}")
 end
 if not fs.exists("sal.lua") then
     local f = io.open("sal.lua", "w")
