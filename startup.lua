c77e159ee14b608abcdbef7d3f9669b743701e3a
RedAngel121 <33588872+RedAngel121@users.noreply.github.com>
2024-03-19 18:32:03 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -4,7 +4,7 @@ if not fs.exists("AddressList.lua") then
 end
 if not fs.exists("sal.lua") then
     local f = io.open("sal.lua", "w")
-    f:write("function saveItemList()\n\tlocal file = io.open(\"AddressList.lua\", \"w\")\n\tif file then\n\t\tfile:write(\"return {\\n\")\n\t\tfor _, item in ipairs(itemList) do\n\t\t\tfile:write(string.format(\"\\t{locName=\\\"%s\\\", address={%s}},\\n\", item.locName, table.concat(item.address, \",\")))\n\t\tend\n\t\tfile:write(\"}\\n\")\n\t\tfile:close()\n\tend\nend\nfunction loadItemList()\n\tlocal file = io.open(\"AddressList.lua\", \"r\")\n\tif file then\n\t\tlocal content = file:read(\"*all\")\n\t\tfile:close()\n\t\tlocal success, loadedItemList = pcall(load(content))\n\t\tif success and type(loadedItemList) == \"table\" then\n\t\t\titemList = loadedItemList\n\t\tend\n\tend\nend")
+    f:write("function saveItemList()\n\tlocal file = io.open(\"AddressList.lua\", \"w\")\n\tif file then\n\t\tfile:write(\"return {\\n\")\n\t\tfor _, item in ipairs(itemList) do\n\t\t\tfile:write(string.format(\"\\t{locName=\\\"%s\\\", address={%s}},\\n\", item.locName, table.concat(item.address, \",\")))\n\t\tend\n\t\tfile:write(\"}\\n\")\n\t\tfile:close()\n\tend\nend\n\nfunction loadItemList()\n\tlocal file = io.open(\"AddressList.lua\", \"r\")\n\tif file then\n\t\tlocal content = file:read(\"*all\")\n\t\tfile:close()\n\t\tlocal success, loadedItemList = pcall(load(content))\n\t\tif success and type(loadedItemList) == \"table\" then\n\t\t\titemList = loadedItemList\n\t\tend\n\tend\nend")
 end
 interface = peripheral.find("basic_interface") or peripheral.find("crystal_interface") or peripheral.find("advanced_crystal_interface")
 if interface == nil then
