d17953317db44b49f8d27daab2259890cc9176c7
Red <redangel121@gmail.com>
2024-03-23 01:31:49 -0400

    Merged the Editor and Dialer onto one script

--- startup.lua
+++ startup.lua
@@ -2,7 +2,7 @@ if not fs.exists("AddressList.lua") then
     local f = io.open("AddressList.lua", "w")
     f:write("return {\n\t{locName=\"Home\", address={1,2,3,4,5,6,7,8,0}}\n}")
 end
-/interface = peripheral.find("basic_interface") or peripheral.find("crystal_interface") or peripheral.find("advanced_crystal_interface")
+interface = peripheral.find("basic_interface") or peripheral.find("crystal_interface") or peripheral.find("advanced_crystal_interface")
 if interface == nil then
     term.clear()
     term.setCursorPos(1, 1)
