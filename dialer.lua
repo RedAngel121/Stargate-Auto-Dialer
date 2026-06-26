11becc16b04742de8606ba8f8d90140cb111b3da
redangel <redangel121@gmail.com>
2026-06-26 13:58:16 -0400

    attempted fix for broken interface issue

--- dialer.lua
+++ dialer.lua
@@ -140,7 +140,10 @@ function dial(address)
             drawFrontEnd(monitor, mh, mw)
         end
         local symbol = address.address[chevron]
-        if stargateType == "sgjourney:milky_way_stargate" then
+        if stargateType ~= "sgjourney:milky_way_stargate" then
+            interface.engageSymbol(symbol)
+            sleep(0.5)
+        else
             if (prevSymbol > symbol and (prevSymbol - symbol) < 19) or (prevSymbol < symbol and (symbol - prevSymbol) > 19) then
             -- if chevron % 2 == 0 then
                 interface.rotateClockwise(symbol)
@@ -154,9 +157,6 @@ function dial(address)
             interface.closeChevron()
             sleep(0.5)
             prevSymbol = symbol
-        else
-            interface.engageSymbol(symbol)
-            sleep(0.5)
         end
     end
     gateIsDialing = false
