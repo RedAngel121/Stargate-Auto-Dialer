8ea10d31403ba69d5287a083189d84258c876561
redangel <redangel121@gmail.com>
2026-08-02 11:01:10 -0400

    fixed for SGJ v6.45+

--- Redstone_Dialing.lua
+++ Redstone_Dialing.lua
@@ -7,28 +7,42 @@ abydos={1,2,3,4,5,6,7,8,0}
 undergarden={1,2,3,4,5,6,7,8,0}
 
 function dial(address)
+    gateIsDialing = true
     local start = interface.getChevronsEngaged() + 1
     local prevSymbol = 0
-    for chevron = start,#address,1 do
-        local symbol = address[chevron]
-        if stargateType == "sgjourney:milky_way_stargate" then
+    for chevron = start,#address.address,1 do
+        drawFrontEnd(term, th, tw)
+        if monitor ~= nil then
+            drawFrontEnd(monitor, mh, mw)
+        end
+        local symbol = address.address[chevron]
+        if stargateType == "sgjourney:milky_way_stargate" or stargateType == "sgjourney:universe_stargate" or stargateType == "sgjourney:classic_stargate" then
             if (prevSymbol > symbol and (prevSymbol - symbol) < 19) or (prevSymbol < symbol and (symbol - prevSymbol) > 19) then
+            -- if chevron % 2 == 0 then
                 interface.rotateClockwise(symbol)
             else
                 interface.rotateAntiClockwise(symbol)
             end
             while(not interface.isCurrentSymbol(symbol)) do sleep(0) end
-            sleep(0.25)
-            interface.openChevron()
-            sleep(0.4)
-            interface.closeChevron()
-            sleep(0.4)
+            sleep(0.3)
+            if stargateType == "sgjourney:milky_way_stargate" then
+                interface.openChevron()
+                sleep(0.5)
+                interface.closeChevron()
+            else
+                interface.encodeChevron()
+            end
+            sleep(0.5)
             prevSymbol = symbol
         else
             interface.engageSymbol(symbol)
-            sleep(0.4)
+            sleep(0.5)
         end
     end
+    if stargateType == "sgjourney:milky_way_stargate" or stargateType == "sgjourney:universe_stargate" or stargateType == "sgjourney:classic_stargate" then
+        interface.engageStargate()
+    end
+    gateIsDialing = false
 end
 
 while true do
